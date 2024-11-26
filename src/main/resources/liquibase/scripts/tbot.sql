6011c2df431df045c69bb3030b9a8bfe379abd83
russa89 <natast2189@gmail.com>
2024-11-26 22:46:25 +0300

    done

--- src/main/resources/liquibase/scripts/tbot.sql
+++ src/main/resources/liquibase/scripts/tbot.sql
@@ -2,22 +2,24 @@
 
 -- changeset nast:1
 CREATE TABLE notification_task (
-id SERIAL PRIMARY KEY,
-chatId INTEGER,
-message TEXT,
-timing TIMESTAMP
+id BIGSERIAL PRIMARY KEY,
+chat_id INTEGER NOT NULL,
+message TEXT NOT NULL,
+message_timing TIMESTAMP NOT NULL
 );
 
 -- changeset nast:2
-SELECT timing AS message_timing
-FROM notification_task;
-
--- changeset nast:3
 ALTER TABLE notification_task
-ALTER COLUMN id TYPE BIGSERIAL,
-ALTER COLUMN chatId SET NOT NULL,
-ALTER COLUMN message SET NOT NULL,
-ALTER COLUMN message_timing SET NOT NULL,
-ADD CONSTRAINT chatId_unique UNIQUE (chatId);
+ALTER COLUMN chat_id TYPE BIGINT;
+
+
+
+
+
+
+
+
+
+
 
 
